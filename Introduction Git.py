4a76f6a58cf8e49e201efdb5a677c7bb521deb9b
owlandauk <50512489+owlandauk@users.noreply.github.com>
2025-05-11 17:02:27 +0200

    Update Introduction Git.py

--- Introduction Git.py
+++ Introduction Git.py
@@ -1,1 +1,16 @@
-pwd 
+pwd  查看目录
+git init 初始化
+
+
+//EDIT
+  //Adding to the staging area
+    git add.
+ // COMMIT
+    git commit -m " "//为所做的修改提交注释
+
+ //log
+ git log //可以查看最近commit
+ git log  --since='Month Day Year'
+
+  //diff
+  git diff 
